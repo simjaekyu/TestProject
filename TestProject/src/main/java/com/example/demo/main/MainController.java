40cfcb7e2bd441418cb9d4b31f163070b54dc46d
simjaekyu <simjaekyu@10.204.36.26>
2019-01-11 16:16:55 +0900

    first commit

---
+++ TestProject/src/main/java/com/example/demo/main/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/main")
+	public String main(Model model) {
+		model.addAttribute("main","is good");
+		return "main";
+	}
+}
